6d1ee8f33d12ffbabaed459f36caca22cfd1298f
Kirill <K-baranov@outlook.com>
2024-01-17 14:34:59 +0100

    Initial Commit

---
+++ ChallengeDay47/ChallengeDay47App.swift
@@ -0,0 +1,17 @@
+//
+//  ChallengeDay47App.swift
+//  ChallengeDay47
+//
+//  Created by Kirill Baranov on 17/01/24.
+//
+
+import SwiftUI
+
+@main
+struct ChallengeDay47App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
